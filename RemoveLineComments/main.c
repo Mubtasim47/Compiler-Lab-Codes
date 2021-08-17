9fea2791f51cd871f9540b21a861ede5f61a548c
Mubtasim47 <88978153+Mubtasim47@users.noreply.github.com>
2021-08-18 03:51:19 +0600

    Update main.c

--- RemoveLineComments/main.c
+++ RemoveLineComments/main.c
@@ -4,16 +4,16 @@
 void remove_comment(char);
 FILE *fpi, *fpo;
 int main() {
- char c;
- fpi = fopen("input.txt", "r");
- fpo = fopen("output.txt", "w");
- while((c = (char) fgetc(fpi)) != EOF)
- remove_comment(c);
- fclose(fpi);
- fclose(fpo);
- return 0;
-
+    char c;
+    fpi = fopen("input.txt", "r");
+    fpo = fopen("output.txt", "w");
+    while((c = (char) fgetc(fpi)) != EOF)
+        remove_comment(c);
+    fclose(fpi);
+    fclose(fpo);
+    return 0;
 }
+
 
 
 void remove_comment (char ch) {
@@ -22,15 +22,15 @@ void remove_comment (char ch) {
         d = ch;
         ch = (char) fgetc(fpi);
         if(ch == '/') {
-            while(fgetc(fpi) !='\n');
-            ch = (char) fgetc(fpi);
+            while((ch = (char) fgetc(fpi)) !='\n');
             fputc(ch, fpo);
             return;
         }
         fputc(d, fpo);
         fputc(ch, fpo);
         return;
-    
+
     }
-    fputc(ch, fpo); 
+    fputc(ch, fpo);
+
 }
