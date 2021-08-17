2e2a7c880db244c1b003ee1e4f7ac01b3cb44b84
Mubtasim47 <88978153+Mubtasim47@users.noreply.github.com>
2021-08-18 03:07:17 +0600

    Create main.c

---
+++ RemoveLineComments/main.c
@@ -0,0 +1,36 @@
+//Remove line comment(s) from a source code
+
+#include <stdio.h>
+void remove_comment(char);
+FILE *fpi, *fpo;
+int main() {
+ char c;
+ fpi = fopen("input.txt", "r");
+ fpo = fopen("output.txt", "w");
+ while((c = (char) fgetc(fpi)) != EOF)
+ remove_comment(c);
+ fclose(fpi);
+ fclose(fpo);
+ return 0;
+
+}
+
+
+void remove_comment (char ch) {
+    char d;
+    if(ch == '/') {
+        d = ch;
+        ch = (char) fgetc(fpi);
+        if(ch == '/') {
+            while(fgetc(fpi) !='\n');
+            ch = (char) fgetc(fpi);
+            fputc(ch, fpo);
+            return;
+        }
+        fputc(d, fpo);
+        fputc(ch, fpo);
+        return;
+    
+    }
+    fputc(ch, fpo); 
+}
