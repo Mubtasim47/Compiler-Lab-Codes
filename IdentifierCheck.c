8a3c651b08d8d0ce63d9edba7201a201e3426417
Mubtasim47 <88978153+Mubtasim47@users.noreply.github.com>
2021-08-18 15:27:17 +0600

    Create IdentifierCheck.c

---
+++ IdentifierCheck.c
@@ -0,0 +1,66 @@
+//program to check whether a given identifier is valid or not
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isAlphabet(char);
+
+bool isNumber(char);
+
+
+int main() {
+
+    char str[50];
+
+    printf("Enter a string:\n");
+    gets(str);
+
+    char keyword[32][10] = {
+            "auto", "double", "int", "struct", "break", "else", "long",
+            "switch", "case", "enum", "register", "typedef", "char",
+            "extern", "return", "union", "const", "float", "short",
+            "unsigned", "continue", "for", "signed", "void", "default",
+            "goto", "sizeof", "volatile", "do", "if", "static", "while"
+    };
+
+  
+    //check for keyword 
+    for (int i = 0; i < 32; i++) {
+        if (!strcmp(str, keyword[i])) {
+            printf("Valid keyword\n");
+            return 0;
+        }
+    }
+
+    bool $validIdentifier = false;
+
+    if (isAlphabet(str[0]) || str[0] == '_') {
+        $validIdentifier = true;
+        int i = 0;
+        while (str[i] != '\0') {
+            if (isAlphabet(str[i]) || isNumber(str[i]) || str[i] == '_' || str[i] == '$') {
+                i++;
+                continue;
+            } else {
+                $validIdentifier = false;
+                break;
+            }
+
+        }
+    }
+
+    printf("%s %s", $validIdentifier ? "Valid" : "Not a valid", "identifier");
+
+    return 0;
+}
+
+
+bool isAlphabet(char ch) {
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+}
+
+bool isNumber(char ch) {
+    return (ch >= '0' && ch <= '9');
+}
+
