7749b8306c0b798407c5b391da0edeced2fa6f90
Mubtasim47 <88978153+Mubtasim47@users.noreply.github.com>
2021-08-18 23:16:00 +0600

    Create StringRecognition.c

---
+++ StringRecognition.c
@@ -0,0 +1,61 @@
+//Program to recognize a string under the regular expression ab(a|b)*abb
+
+#include <stdio.h>
+
+#define END_STATE 'X'
+
+int main() {
+
+    char str[50];
+    char state = 'A'; //A as initial state
+
+    printf("Enter the string:\n");
+    gets(str);
+
+    int i = -1;
+    while(str[++i] != '\0') {
+        printf("i = %d\n", i);
+
+        switch (state) {
+
+            case 'A':
+                if(str[i] == 'a') state = 'B';
+                else state = END_STATE;
+                break;
+            case 'B':
+                if(str[i] == 'b') state = 'C';
+                else state = END_STATE;
+                break;
+            case 'C':
+                if(str[i] == 'a') state = 'D';
+                else if(str[i] == 'b') state = 'E';
+                else state = END_STATE;
+                break;
+            case 'D':
+                if(str[i] == 'a') state = 'D';
+                else if(str[i] == 'b') state = 'F';
+                else state = END_STATE;
+                break;
+            case 'E':
+                if(str[i] == 'a') state = 'D';
+                else if(str[i] == 'b') state = 'E';
+                else state = END_STATE;
+                break;
+            case 'F':
+                if(str[i] == 'a') state = 'D';
+                else if(str[i] == 'b') state = 'G';
+                else state = END_STATE;
+                break;
+            case 'G':
+                if(str[i] == 'a') state = 'D';
+                else if(str[i] == 'b') state = 'E';
+                else state = END_STATE;
+                break;
+            default: //if end state occurs
+                break;
+        }
+    }
+
+    printf("%s", (state == 'G') ? "Accepted" : "Rejected");
+    return 0;
+}
